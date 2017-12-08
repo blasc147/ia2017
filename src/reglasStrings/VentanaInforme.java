9f2e13fc0b8200e34fa68f242e591e67e7261dde
Gerardo Budzovsky <gerardo.dav.bud@gmail.com>
2017-12-08 17:03:44 -0300

    JRadioButtons

--- src/reglasStrings/VentanaInforme.java
+++ src/reglasStrings/VentanaInforme.java
@@ -8,20 +8,44 @@ package reglasStrings;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.util.Vector;
+
+import javax.swing.ButtonGroup;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+import javax.swing.JRadioButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**Este clase representa la ventana
  * emergente donde se muestran las reglas
  * de asociaci�n de manera tabulada *
  * @author Grupo9-IA2017
  */
-class VentanaInforme extends JDialog {
+
+class VentanaInforme extends JDialog implements ChangeListener{
    
+	private Container c;
+	private JRadioButton rdbtnMuyConfiable;
+	private JRadioButton rdbtnConfiable;
+	private JRadioButton rdbtnPocoConfiable;
+	private JRadioButton rdbtnNoConfiable;
+	private ButtonGroup bg;
+	private JLabel lblNivelesDeConfiabilidad;
+	private JLabel lblNewLabel;
+	private JTable table;
+	private JTable table2;
+	private JTable table3;
+	private JTable table4;
+	private JScrollPane scrollPane;
+
+	
 	/**
 	 * Este m�todo constructor nos dice como
 	 * ser� la creaci�n de la instancia de la 
@@ -105,17 +129,81 @@ class VentanaInforme extends JDialog {
                 }
             };
 
-            JTable table = new JTable( model );
-            JTable table2 = new JTable( model2 );
-            JTable table3 = new JTable( model3 );
-            JTable table4 = new JTable( model4 );
+            table = new JTable( model );
+            table2 = new JTable( model2 );
+            table3 = new JTable( model3 );
+            table4 = new JTable( model4 );
             table.setAutoCreateRowSorter(true);
-            Container c = getContentPane();
+            table2.setAutoCreateRowSorter(true);
+            table3.setAutoCreateRowSorter(true);
+            table4.setAutoCreateRowSorter(true);
+
+            
+            c = getContentPane();
             c.setLayout( new FlowLayout() );
-            add(new JScrollPane(table));
+
+            scrollPane = new JScrollPane(table);
+            add(scrollPane); 
+            
+        	bg=new ButtonGroup();
+            
+            rdbtnMuyConfiable = new JRadioButton("Muy Confiable");
+            //rdbtnMuyConfiable.setBounds(449, 26, 109, 23);
+            rdbtnMuyConfiable.addChangeListener(this);
+            getContentPane().add(rdbtnMuyConfiable);
+            bg.add(rdbtnMuyConfiable);
+            
+            rdbtnConfiable = new JRadioButton("Confiable");
+            //rdbtnConfiable.setBounds(449, 52, 109, 23);
+            rdbtnConfiable.addChangeListener(this);
+            getContentPane().add(rdbtnConfiable);
+            bg.add(rdbtnConfiable);
+
+            
+            rdbtnPocoConfiable = new JRadioButton("Poco confiable");
+//          rdbtnPocoConfiable.setBounds(449, 78, 109, 23);
+            rdbtnPocoConfiable.addChangeListener(this);
+            getContentPane().add(rdbtnPocoConfiable);
+            bg.add(rdbtnPocoConfiable);
+
+            
+            rdbtnNoConfiable = new JRadioButton("No confiable");
+//          rdbtnNoConfiable.setBounds(449, 104, 109, 23);
+            rdbtnNoConfiable.addChangeListener(this);
+            getContentPane().add(rdbtnNoConfiable);
+            bg.add(rdbtnNoConfiable);
+            
+            lblNivelesDeConfiabilidad = new JLabel("Niveles de confiabilidad");
+            lblNivelesDeConfiabilidad.setFont(new Font("Tahoma", Font.BOLD, 12));
+            lblNivelesDeConfiabilidad.setBounds(453, 5, 146, 14);
+            getContentPane().add(lblNivelesDeConfiabilidad);
+            
+            lblNewLabel = new JLabel("Aca va el texto");
+            lblNewLabel.setBounds(453, 134, 172, 30);
+            getContentPane().add(lblNewLabel);
+            
             this.pack();
             this.show();        
+        }
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		
+        if (rdbtnMuyConfiable.isSelected()) {
+        	scrollPane.setViewportView(table);
+        }
+        if (rdbtnConfiable.isSelected()) {
+        	scrollPane.setViewportView(table2);
+
+        }
+        if (rdbtnPocoConfiable.isSelected()) {
+        	scrollPane.setViewportView(table3);
+
         } 
         
-        
+        if (rdbtnNoConfiable.isSelected()) {
+        	scrollPane.setViewportView(table4);
+
+        }		
+	} 
 }
